384c195e9002a91831705a437978cdc9b878b3ef
林有勋 <linyouxun@yoju360.com>
2019-03-07 10:15:59 +0800

    【修改】数据库

--- src/sequelizeConfig.ts
+++ src/sequelizeConfig.ts
@@ -1,20 +1,20 @@
 import { Sequelize } from 'sequelize-typescript';
 
-// export const sequelize = new Sequelize({
-    // dialect: 'mysql',
-    // host: '121.41.58.94',
-    // port: 3307,
-    // operatorsAliases: true,
-    // database: 'intelligent',
-    // username: 'yunwei',
-    // password: '!@#_20150414',
-    // modelPaths: [__dirname + '/models']
-// })
 export const sequelize = new Sequelize({
     dialect: 'mysql',
+    host: '121.41.58.94',
+    port: 3307,
     operatorsAliases: true,
-    database: 'yoju',
-    username: 'root',
-    password: '123456l',
+    database: 'intelligent',
+    username: 'yunwei',
+    password: '!@#_20150414',
     modelPaths: [__dirname + '/models']
 })
+// export const sequelize = new Sequelize({
+    // dialect: 'mysql',
+    // operatorsAliases: true,
+    // database: 'yoju',
+    // username: 'root',
+    // password: '123456l',
+    // modelPaths: [__dirname + '/models']
+// })
