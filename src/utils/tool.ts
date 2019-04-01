b8ea27b08c3967084cf3938770fa22cc811d1c73
林有勋 <linyouxun@yoju360.com>
2019-04-01 12:40:08 +0800

    【修改】token获取方式

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -2,8 +2,8 @@
 import * as crypto from 'crypto';
 import { WX_TOKEN } from './const';
 export const checkToken = function(signature, timestamp, nonce) {
-    const key = crypto.createHash('sha1').update([WX_TOKEN, timestamp, nonce].sort().join()).digest('hex').toUpperCase();
-    if(key === signature) {
+    const key = crypto.createHash('sha1').update([WX_TOKEN, timestamp, nonce].sort().join("")).digest('hex').toUpperCase();
+    if(key === signature.toUpperCase()) {
         return true;
     } else {
         return false;
