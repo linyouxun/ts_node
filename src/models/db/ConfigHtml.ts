2d7926347ee9eee10a3cfbcd9fb54dfd14e328df
林有勋 <linyouxun@yoju360.com>
2019-04-30 14:49:21 +0800

    [修改]添加字段

--- src/models/db/ConfigHtml.ts
+++ src/models/db/ConfigHtml.ts
@@ -1,6 +1,10 @@
-import { Table, Column, Model, PrimaryKey, AutoIncrement, HasMany } from 'sequelize-typescript';
+import { Table, Column, Model, PrimaryKey, AutoIncrement, HasMany, DataType } from 'sequelize-typescript';
 import Statistics from './Statistics';
 
+enum types {
+    miniapp = '小程序', wx = '公众号', web = 'web',
+    other = '其他', app = 'app', sort = '桌面软件'
+}
 /**
  * 统计
  */
@@ -16,6 +20,13 @@ export default class ConfigHtml extends Model<ConfigHtml> {
     id: number;
     @Column({comment: '名字', field: 'title'})
     title: string;
+    @Column({
+        comment: '类型', 
+        field: 'type',
+        type: DataType.ENUM(types.miniapp, types.wx, types.web, types.other, types.app, types.sort),
+    })
+    type: string;
+
     @HasMany(() => Statistics)
     statistics: Statistics[];
 }
